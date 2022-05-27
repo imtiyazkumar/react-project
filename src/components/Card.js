0a094e9edf85bda1f894df0ec05a7efef4f8d0be
Your Name <imtiyazah907@gmail.com>
2022-05-28 00:41:17 +0530

    k

---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import './Card.css';
+function Card (props) {
+    const classes = 'card '+ props.className;
+    return <div className = {classes}> {props.Children} </div>
+}
+
+export default Card;
---
+++ src/components/TestItem.js
@@ -0,0 +1,7 @@
+function TestItem() {
+    return (
+        <div><h1>hello</h1></div>
+    );
+}
+
+export default TestItem;
